5b930fa79379187b67d190bca61af818adf86e7b
Vlad-Iliescu <vlad.iliescu.88@gmail.com>
2021-07-02 22:36:52 +0300

    Add namespace support

--- index.js
+++ index.js
@@ -4,19 +4,58 @@ const fp = require('fastify-plugin')
 const RedisMock = require('ioredis-mock')
 
 function fastifyRedisMock (fastify, options, next) {
-  var client = options.client || null
+  const { namespace, url, ...redisOptions } = options
+  let client = options.client || null
 
-  if (!client) {
-    try {
-      client = new RedisMock(options)
-    } catch (err) {
-      return next(err)
+  if (namespace) {
+    if (!fastify.redis) {
+      fastify.decorate('redis', {})
+    }
+
+    if (fastify.redis[namespace]) {
+      return next(new Error(`Redis '${namespace}' instance namespace has already been registered`))
+    }
+
+    const closeNamedInstance = (fastify, done) => {
+      fastify.redis[namespace].quit(done)
+    }
+
+    if (!client) {
+      try {
+        client = new RedisMock(redisOptions)
+      } catch (err) {
+        return next(err)
+      }
+
+      fastify.addHook('onClose', closeNamedInstance)
+    }
+
+    fastify.redis[namespace] = client
+
+    if (options.closeClient === true) {
+      fastify.addHook('onClose', closeNamedInstance)
+    }
+  } else {
+    if (fastify.redis) {
+      return next(new Error('fastify-redis has already been registered'))
+    } else {
+      if (!client) {
+        try {
+          client = new RedisMock(redisOptions)
+        } catch (err) {
+          return next(err)
+        }
+
+        fastify.addHook('onClose', close)
+      }
+
+      fastify.decorate('redis', client)
+
+      if (options.closeClient === true) {
+        fastify.addHook('onClose', close)
+      }
     }
   }
-
-  fastify
-    .decorate('redis', client)
-    .addHook('onClose', close)
 
   next()
 }
